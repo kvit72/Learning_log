ef467684d6238c07e9fa5a10a990432689318f3e
kvit7 <kvit72v@yandex.ru>
2022-12-27 10:53:24 +0300

    Сопоставление URL. Написание представления. Написание шаблона.

---
+++ learning_logs/urls.py
@@ -0,0 +1,10 @@
+"""Определяет схемы URL для learning_logs."""
+
+from django.urls import path
+from . import views
+
+app_name = 'learning_logs'
+urlpatterns = [
+    # Домашняя страница
+    path('', views.index, name='index'),
+]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    """Домашняя страница приложения Learning Log"""
+    return render(request, 'learning_logs/index.html')
