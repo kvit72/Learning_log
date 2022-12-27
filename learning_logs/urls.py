9a142a6641d2741d2de7b001c8dda58e5273c844
kvit7 <kvit72v@yandex.ru>
2022-12-27 12:02:03 +0300

    Создание страницы со списком тем.

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -7,4 +7,6 @@ app_name = 'learning_logs'
 urlpatterns = [
     # Домашняя страница
     path('', views.index, name='index'),
+    # Страница со списком всем тем.
+    path('topics/', views.topics, name='topics'),
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import Topic
 
 def index(request):
     """Домашняя страница приложения Learning Log"""
     return render(request, 'learning_logs/index.html')
+
+def topics(request):
+    """Выводит список тем."""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
