65cb15316509644925776f9c04e49abf60cda269
kvit7 <kvit72v@yandex.ru>
2022-12-30 00:45:37 +0300

    Создание страницы регистрации.

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,12 @@
 """Определяет схемы URL для пользователей"""
 
 from django.urls import path, include
+from . import views
 
 app_name = 'users'
 urlpatterns = [
     # Включить URL авторизации по умолчанию.
     path('', include('django.contrib.auth.urls')),
+    # Страница регистрации.
+    path('register/', views.register, name='register'),
 ]
