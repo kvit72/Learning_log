20f1331fbe090c3e232af0c1d20c046b33b95f48
kvit7 <kvit72v@yandex.ru>
2022-12-29 23:05:32 +0300

    Шаблон new_topic.

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     path('topics/', views.topics, name='topics'),
     # Страница с подробной информацией по отдельной теме.
     path('topics/<int:topic_id>/', views.topic, name='topic'),
+    # Страница для добавления новой темы
+    path('new_topic/', views.new_topic, name='new_topic'),
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Topic
+from .forms import TopicForm
 
 def index(request):
     """Домашняя страница приложения Learning Log"""
@@ -17,3 +18,19 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+    """Определяет новую тему."""
+    if request.method != 'POST':
+        # Данные не отправлялись; создается пустая форма.
+        form = TopicForm()
+    else:
+        # Отправлены данные POST; обработать данные.
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs: topics')
+
+    # Вывести пустую или недействительную форму.
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
